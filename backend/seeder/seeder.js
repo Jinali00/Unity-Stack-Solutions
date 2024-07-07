28fdd5362347656096944821b1915fb1fb0b19fc
jinali-vekariya <jinalivekaria00@gmail.com>
2024-07-07 15:21:33 -0400

    products data created in database and added & search api created

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -1,23 +1,21 @@
-
-import mongoose from 'mongoose';
-import Product from '../models/product.js';    
-import products from './data.js';
+const mongoose = require("mongoose");
+const Product = require("../models/product.model");
+const products = require("./data");
 
 const seedProducts = async () => {
-    try{
-        await mongoose.connect("mongodb://localhost:27017/unity-stack-solutions");
-        await Product.deleteMany();
-        console.log('Products are deleted');
+  try {
+    await mongoose.connect("mongodb://localhost:27017/unity-stack-solutions");
+    await Product.deleteMany();
+    console.log("Products are deleted");
 
-        await Product.insertMany(products);
-        console.log('All products are added');
+    await Product.insertMany(products);
+    console.log("All products are added");
 
-        process.exit();
-
-    } catch (error) {
-        console.log(error.message);
-        process.exit();
-    }
-}
+    process.exit();
+  } catch (error) {
+    console.log(error.message);
+    process.exit();
+  }
+};
 
 seedProducts();
---
+++ backend/v1/controller/product.controller.js
@@ -0,0 +1,89 @@
+const constants = require("../../config/constants");
+const Product = require("../../models/product.model");
+const data = require("../../seeder/data");
+const { sendResponse } = require("../../services/common.services");
+
+const getAllProduct = async (req, res, next) => {
+  try {
+    let { limit, page, sortBy, search } = req.query;
+
+    if (limit) {
+      limit = +limit || constants.LIMIT;
+    }
+    if (page) {
+      page = +page || constants.PAGE;
+    }
+
+    let query = {};
+    let field = "created_at",
+      value = 1;
+    if (sortBy) {
+      const parts = sortBy.split(":");
+      field = parts[0];
+      parts[1] === "desc" ? (value = -1) : (value = 1);
+    }
+    if (search) {
+      query.$or = [
+        { name: new RegExp(search, "i") },
+        { category: new RegExp(search, "i") },
+        { seller: new RegExp(search, "i") },
+      ];
+    }
+    const total = await Product.countDocuments(query);
+
+    if (total === 0) {
+      return sendResponse(
+        res,
+        constants.WEB_STATUS_CODE.OK,
+        constants.STATUS_CODE.SUCCESS,
+        "GENERAL.GET_LIST",
+        { data: null, total, limit, page },
+        req.headers.lang
+      );
+    }
+
+    const product = await Product.find(query)
+      .limit(limit)
+      .skip(limit * (page - 1))
+      .sort({ [field]: value });
+
+    return sendResponse(
+      res,
+      constants.WEB_STATUS_CODE.OK,
+      constants.STATUS_CODE.SUCCESS,
+      "GENERAL.GET_LIST",
+      { data: product, total, limit: limit || null, page: page || null },
+      req.headers.lang
+    );
+  } catch (err) {
+    console.log("Error(getAllProduct)..", err);
+    return sendResponse(
+      res,
+      constants.WEB_STATUS_CODE.SERVER_ERROR,
+      constants.STATUS_CODE.FAIL,
+      "GENERAL.GENERAL_ERROR_CONTENT",
+      { message: err.message },
+      req.headers.lang
+    );
+  }
+};
+
+const addProduct = async (req, res, next) => {
+  try {
+    // data.map(async (data) => await Product.create(data));
+    const product = await Product.create(req.body);
+
+    return res.send({ message: "Product added successfully.", product });
+  } catch (err) {
+    console.log("Error(addProduct)..", err);
+    return sendResponse(
+      res,
+      constants.WEB_STATUS_CODE.SERVER_ERROR,
+      constants.STATUS_CODE.FAIL,
+      "GENERAL.GENERAL_ERROR_CONTENT",
+      { message: err.message },
+      req.headers.lang
+    );
+  }
+};
+module.exports = { getAllProduct, addProduct };
